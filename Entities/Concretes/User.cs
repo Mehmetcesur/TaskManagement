b23cae48cf82774b28afad734b37bd7f67cb5525
burakugras <burakugras67@gmail.com>
2024-08-06 20:14:39 +0300

    code trying

--- Entities/Concretes/User.cs
+++ Entities/Concretes/User.cs
@@ -14,6 +14,7 @@ namespace Entities.Concretes
         public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
         public List<Duty> Duties { get; set; }
+        //hello world!!
 
     }
 }
